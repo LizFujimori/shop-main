6fd22eed02302170affa7e77dcb9e889958282ad
LizFujimori <liizfdaan@gmail.com>
2024-06-19 16:08:42 -0400

    loading

--- src/app/components/Spinner.js
+++ src/app/components/Spinner.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Spinner() {
     return (
-        <Image width={100} height={100} alt="" src={"/vercel.svg"}/>
+        <Image width={100} height={100} alt="" src={"/carregando.svg"}/>
     )
 }
